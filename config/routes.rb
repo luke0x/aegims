a0d0e9192ec9471bd74a1ca749d7b6ff2a7ca278
Luke Crawford <luke0x@gmail.com>
2013-12-28 15:44:10 -0800

    Add Griddler route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Aegims::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	post '/email_processor' => 'griddler/emails#create'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
