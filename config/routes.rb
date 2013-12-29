bb3c0ff1e400d807bedca2b26a0d33b1b0ffaf14
Luke Crawford <luke0x@gmail.com>
2013-12-28 16:09:38 -0800

    Add post index routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Aegims::Application.routes.draw do
 	post '/email_processor' => 'griddler/emails#create'
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :posts
+  root "posts#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
