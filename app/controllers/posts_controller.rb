0be60289d8a83105ef8364812dee62e4ce017b11
Luke Crawford <luke0x@gmail.com>
2013-12-28 16:46:23 -0800

    Send loremimage for now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
+require "open-uri"
+
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+  end
+
+  def show
+    send_data open("http://lorempixel.com/800/800/people/").read, disposition: "inline"
   end
 end
